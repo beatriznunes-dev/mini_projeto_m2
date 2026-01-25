3542a9819a00c210c457fe5dfac4d1e79ce3abd0
beatriznunes-dev <beatriznunnes.d@gmail.com>
2026-01-25 15:06:36 -0300

    Configura API para deploy no Render

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 process.stdout.write('\uFEFF');
 
 require('dotenv').config();
-const express = require("express");
+const express = require('express');
+const cors = require('cors');
+
 const sequelize = require('./database');
 const tarefaRoutes = require('./routes/tarefaRoutes');
 
 const app = express();
+
 app.use(express.json());
+app.use(cors());
+
 app.use('/api', tarefaRoutes);
 
 const PORT = process.env.PORT || 3000;
@@ -20,4 +25,4 @@ sequelize.sync({ force: false })
   })
   .catch(error => {
     console.error('Erro ao sincronizar banco:', error);
-  });
+  })
